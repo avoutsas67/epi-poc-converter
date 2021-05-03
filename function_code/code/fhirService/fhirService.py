ed178083de376ccaf351762b25fa1c519e763b7d
P Sagar <psaga@microsoft.com>
2021-05-03 12:22:25 +0530

    Submit to server changes

--- function_code/code/fhirService/fhirService.py
+++ function_code/code/fhirService/fhirService.py
@@ -10,11 +10,6 @@ class FhirService:
     def __init__(self, body):
         self.body = body
 
-    def cleanRawData(self):
-        self.body = self.body.replace(u"\u2011", '-')
-        self.body = self.body.replace(u"\u201c", '"')
-        
-
     def storeXMLIdOnPost(self, post_xml_id):
         file_path = os.path.abspath(os.path.join('..'))
         file_path = os.path.join(file_path, 'data')
@@ -26,7 +21,6 @@ class FhirService:
             
             xml_id = defaultdict(list)
             xml_id['ID']= [post_xml_id ]
-            # xml_id['TimeStamp']=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
             xml_id_json = {
                 'data':str([xml_id])
                 }
@@ -40,7 +34,6 @@ class FhirService:
             with open(file_path) as f:
                 post_data = json.load(f)
             f.close()
-            print(post_data)
             df = pd.DataFrame(post_data['data'])
             display(df.head(5))
             for i, row in enumerate(df.itertuples(), 0):
@@ -51,7 +44,6 @@ class FhirService:
             display(df.head(5))
             if(not id_found):
                 df.loc[len(df.index)] = [post_xml_id, datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")] 
-            display(df.head(5))
            
             with open(file_path, 'w+') as outfile:
                 json.dump({'data':df.to_json(orient="records")}, outfile)
@@ -59,7 +51,6 @@ class FhirService:
 
     def submitFhirXml(self):
         url = "https://ema-dap-epi-dev-fhir-api.azurewebsites.net/Bundle"
-        self.cleanRawData()
 
         response = requests.post(url, data=self.body, headers={'Content-Type': 'application/fhir+xml; charset=utf-8'})
         
@@ -67,7 +58,6 @@ class FhirService:
             response.raise_for_status()
             response = response.json()
             print('POST sucessful: XML added with id', response['id'])
-            # self.storeXMLIdOnPost(response['id'])
         except HTTPError as http_err:
             response = response.json()
             print(f'HTTP error occurred: {http_err}')
